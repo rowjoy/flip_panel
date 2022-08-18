0fc85f291ef92c3f1946c6ae26262d5e9e276a6b
Jamirul islam <rowjoy202@gmail.com>
2022-08-18 18:15:32 +0600

    Flip card system design

---
+++ android/app/src/main/kotlin/com/example/card_flip_animations/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.card_flip_animations
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sizer/sizer.dart';
+
+import 'card_flip/flip_animation.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Sizer(builder: (context, orientation, deviceType) {
+      return MaterialApp(
+        title: 'Animation',
+        debugShowCheckedModeBanner: false,
+        theme: themeData(
+          ThemeData(
+            brightness: Brightness.dark,
+            backgroundColor: Colors.black,
+            primarySwatch: Colors.blue,
+          ),
+        ),
+        home: FlipAnimation(),
+      );
+    });
+  }
+}
+
+// Apply font to our app's theme
+ThemeData themeData(ThemeData theme) {
+  return theme.copyWith(
+    textTheme: GoogleFonts.corbenTextTheme(
+      theme.textTheme,
+    ),
+  );
+}
